ff7020d4ffdc38af65f79bf064fa65d9302be549
samycici <samycici@gmail.com>
2016-07-09 17:44:38 -0300

    Melhorando mensagem do matcher customizado

--- features/support/matchers/custom.rb
+++ features/support/matchers/custom.rb
@@ -5,5 +5,8 @@ RSpec::Matchers.define :all_list_elements_eq do |expected|
     actual.each do |i|
       expect(i).to eq(expected)
     end
-  end  
+  end
+  failure_message_for_should do |actual|
+    "expected that all elements in #{actual} list would be equals #{expected}"
+  end
 end
